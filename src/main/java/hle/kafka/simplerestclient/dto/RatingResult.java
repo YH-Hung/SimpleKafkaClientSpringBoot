ea8167738b041714f72e26f183549e3382b5b93e
Ying-Han Hung <hle.infdy@gmail.com>
2022-07-01 14:06:29 +0800

    Jackson case-insensitive

--- src/main/java/hle/kafka/simplerestclient/dto/RatingResult.java
+++ src/main/java/hle/kafka/simplerestclient/dto/RatingResult.java
@@ -1,5 +1,6 @@
 package hle.kafka.simplerestclient.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import hle.kafka.simplerestclient.model.ScoreRating;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public class RatingResult {
     String name;
     Integer mergedScore;
--- src/main/java/hle/kafka/simplerestclient/dto/ScoreResult.java
+++ src/main/java/hle/kafka/simplerestclient/dto/ScoreResult.java
@@ -1,10 +1,12 @@
 package hle.kafka.simplerestclient.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public class ScoreResult {
 
     String name;
